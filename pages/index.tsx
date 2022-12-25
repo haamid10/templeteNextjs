c826046d8450df7abff0eeeeb6994d17dbef3a47
haamid10 <apdixamiid919@gmail.com>
2022-12-25 09:56:40 +0300

    addin home page and some fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Head from 'next/head'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {
+  AiFillTwitterCircle,
+   AiFillLinkedin,
+   AiFillGithub
+  } from 'react-icons/ai'
+  import Image from 'next/image'
+  import Si from "../public/Sii.png"
 
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
     <>
       <Head>
@@ -12,8 +21,37 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=' bg-gray-900'>
-        <h1>siuu</h1>  </main>
+      <main className='bg-white px-10'>
+        <section className='  min-h-screen '>
+          <nav className='py-10 mb-12 flex justify-between '>
+            <h1 className='text-xl font-burtons '>Haamid apdi</h1>
+            <ul className='flex items-center'>
+              <li><BsFillMoonStarsFill className='cursor-pointer'/></li>
+              <li><a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 font-suii font-bold' href="">Resume</a></li>
+            </ul>
+          </nav>
+          <div className=' text-center p-10 '>
+            <h2 className='text-5xl py-2 text-teal-500 font-medium'>
+              Haamid apdi
+            </h2>
+            <h3 className='text-2xl py-2'>FullStack developer</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, architecto. Possimus unde natus non consequuntur consectetur quos totam obcaecati dicta libero aperiam laudantium numquam sed, est perspiciatis dignissimos reiciendis! Libero.
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-800'>
+            <AiFillGithub/>
+            <AiFillLinkedin/>
+            <AiFillTwitterCircle/>
+          </div>
+          <div className='relative'>
+               <Image src={Si} alt="do"/> 
+                
+              
+          </div>
+        </section>
+         </main>
     </>
   )
 }
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       'suii':['Poppins', 'sans-serif'],
+       burtons:"burtons",
+      },
+    },
   },
   plugins: [],
 }
